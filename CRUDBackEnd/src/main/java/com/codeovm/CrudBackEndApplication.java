f0de1a9421d157208e79b75c32b3df8b611edaa3
oscar310394 <oscar310394@gmail.com>
2018-01-29 10:26:52 -0600

    Adding Angular project

--- CRUDBackEnd/src/main/java/com/codeovm/CrudBackEndApplication.java
+++ CRUDBackEnd/src/main/java/com/codeovm/CrudBackEndApplication.java
@@ -1,12 +1,28 @@
 package com.codeovm;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CrudBackEndApplication {
+import com.codeovm.entities.User;
+import com.codeovm.repositories.UserRepository;
 
+@SpringBootApplication
+public class CrudBackEndApplication implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CrudBackEndApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		userRepository.save(new User("one","one"));
+		userRepository.save(new User("two","two"));
+		userRepository.save(new User("three","three"));
 	}
 }
--- CRUDBackEnd/src/main/java/com/codeovm/controller/UserController.java
+++ CRUDBackEnd/src/main/java/com/codeovm/controller/UserController.java
@@ -3,8 +3,11 @@ package com.codeovm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,21 @@ public class UserController {
 	@GetMapping("/user/{id}")
 	public User getUser(@PathVariable int id) {
 		return userRepository.findOne(id);
+	}
+
+	@DeleteMapping("/user/{id}")
+	public boolean deleteUser(@PathVariable int id) {
+		userRepository.delete(id);
+		return true;
+	}
+
+	@PostMapping("/user")
+	public User createUser(User user) {
+		return userRepository.save(user);
+	}
+
+	@PutMapping
+	public User updateUser(User user) {
+		return userRepository.save(user);
 	}
 }
