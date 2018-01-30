714b892e790bf1534029384242b10234a4919b73
oscar310394 <oscar310394@gmail.com>
2018-01-30 00:25:03 -0600

    Creation of service and component list, and user.

--- CRUDBackEnd/src/main/resources/static/restClient/src/app/app.module.ts
+++ CRUDBackEnd/src/main/resources/static/restClient/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListuserComponent } from './components/listuser/listuser.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserService } from './shared-service/user.service';
 
+const appRoutes: Routes = [
+  { path: '', component: ListuserComponent },
+  { path: 'op', component: UserFormComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListuserComponent,
+    UserFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
